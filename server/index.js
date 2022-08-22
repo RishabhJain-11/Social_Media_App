e0b2e0b55fd6886a9731a49405c859d0c0d3ab2d
Rishabh Jain <jainrishabh71011@gmail.com>
2022-08-22 16:16:05 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import postRoutes from './routes/posts.js';
+// import postRoutes from './routes/posts.js';
 
 const app = express();
 
@@ -12,14 +12,17 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts', postRoutes);
+// app.use('/posts', postRoutes);
 
 
-// const CONNECTION_URL = ;
-const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = 'mongodb+srv://<username>:<password>@cluster0.deveiwz.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', false);
+
+//Rishabh11
+//mCR8XNfmE5t4jopM
