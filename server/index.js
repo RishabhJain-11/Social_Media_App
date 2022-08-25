0f315e81dd3856fe55fb6c7f3e9e5a8c2732be1d
Rishabh Jain <jainrishabh71011@gmail.com>
2022-08-25 16:48:41 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-// import postRoutes from './routes/posts.js';
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
@@ -12,10 +12,10 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-// app.use('/posts', postRoutes);
+app.use('/posts', postRoutes);
 
 
-const CONNECTION_URL = 'mongodb+srv://<username>:<password>@cluster0.deveiwz.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://Rishabh123:Rishabh123@cluster0.wi2mbif.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
